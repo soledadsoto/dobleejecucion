18f0e22592998299451e36da277daf523d6a6ae4
soledadsoto <fpsoto2@gmail.com>
2018-05-14 00:51:29 +0200

    Creación rama polaris. Bandera Japón.

--- src/DibujoPolares.java
+++ src/DibujoPolares.java
@@ -12,42 +12,28 @@ public class DibujoPolares {
         //El angulo empieza en la parte superior de la vertical y va en sentido antihorario
 
 
-        if (angulo <= 0.25 || angulo >= 0.75) {
+        if (angulo <= 0.25 || angulo >= 0.20) {
             if (radio < 0.1) {
-                return new Color(147, 147, 147);
+                return new Color(254, 000, 000);
 
             }
-            if (radio > 0.1 && radio < 0.2) {
-                return new Color(136, 24, 246);
+           if (radio > 0.1 && radio < 0.2) {
+                return new Color(254, 000, 000);
 
             }
             if (radio > 0.2 && radio < 0.3) {
-                return new Color(21, 22, 255);
+                return new Color(254, 000, 000);
 
             }
             if (radio > 0.3 && radio < 0.4) {
-                return new Color(118, 238, 255);
+                return new Color(254, 000, 000);
 
             }
-            if (radio > 0.4 && radio < 0.5) {
-                return new Color(36, 255, 102);
 
-            }
-            if (radio > 0.5 && radio < 0.6) {
-                return new Color(253, 255, 57);
 
-            }
-            if (radio > 0.6 && radio < 0.7) {
-                return new Color(255, 135, 30);
-
-            }
-            if (radio > 0.7 && radio < 0.8) {
-                return new Color(255, 21, 34);
-
-            }
 
         }
-        return new Color(147, 147, 147);
+        return new Color(255, 255, 255);
     }
 
 
